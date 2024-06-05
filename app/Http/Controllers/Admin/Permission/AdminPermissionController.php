f19d45ce33d6841b98c63903ec1c9e5c34850329
varunrnc <barunpandit143@gmail.com>
2024-06-05 16:47:27 +0530

    update

--- app/Http/Controllers/Admin/Permission/AdminPermissionController.php
+++ app/Http/Controllers/Admin/Permission/AdminPermissionController.php
@@ -9,6 +9,6 @@ class AdminPermissionController extends Controller
 {
     public function index()
     {
-        return view('admin.permission.index');
+        return view('permission.index');
     }
 }
--- app/Http/Controllers/Admin/Team/AdminTeamController.php
+++ app/Http/Controllers/Admin/Team/AdminTeamController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class AdminTeamController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.team.index');
+    }
 }
